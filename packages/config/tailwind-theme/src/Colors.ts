74de23c35594a297f4573d71ce0695c3213d03c2
Dennis <dennis.spierenburg@hotmail.com>
2022-10-31 16:27:42 +0100

    feat: Kebab case from tokens

--- packages/config/tailwind-theme/src/Colors.ts
+++ packages/config/tailwind-theme/src/Colors.ts
@@ -1,9 +1,14 @@
-// import tokens from "@dipsaus9/design-tokens"
+/* eslint-disable no-restricted-syntax */
+import kebabCase from "lodash.kebabcase"
+
+import * as colors from "@dipsaus9/design-tokens/dist/colors"
+
+const formattedColors: Record<string, string> = {}
+
+for (const [key, value] of Object.entries(colors)) {
+  formattedColors[kebabCase(key)] = value
+}
 
 export const Colors = {
-  primary: {
-    100: "red",
-    200: "green",
-  },
-  // ...Object.values(tokens.color).map()
+  ...formattedColors,
 }
--- packages/config/tailwind-theme/vite.config.ts
+++ packages/config/tailwind-theme/vite.config.ts
@@ -2,16 +2,11 @@ import path from "path"
 
 import { defineConfig } from "vite"
 
-const isExternal = (id: string) => !id.startsWith(".") && !path.isAbsolute(id)
-
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(__dirname, "src/main.ts"),
       name: "tailwind-theme",
-    },
-    rollupOptions: {
-      external: isExternal,
     },
   },
 })
