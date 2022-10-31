da85f2ca6ce2253eefe1ce1b54fc0a521116507c
Dennis <dennis.spierenburg@hotmail.com>
2022-10-31 17:02:54 +0100

    feat: Caching

--- packages/ui/button/src/Button.tsx
+++ packages/ui/button/src/Button.tsx
@@ -8,7 +8,7 @@ export function Button({
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className={`${className} text-color-base-green`}
+      className={`${className} text-color-font-base`}
       {...props}
       type={type}
     >
