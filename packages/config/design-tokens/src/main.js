16ad5c655a458709a022f63ed8d38fb85914955c
Dennis <dennis.spierenburg@hotmail.com>
2022-10-31 15:01:33 +0100

    feat: Design tokens example

---
+++ packages/config/design-tokens/src/main.js
@@ -0,0 +1,159 @@
+/**
+ * Do not edit directly
+ * Generated on Mon, 31 Oct 2022 14:00:34 GMT
+ */
+
+module.exports = {
+  color: {
+    base: {
+      gray: {
+        light: {
+          value: "#CCCCCC",
+          filePath: "tokens/colors/base.json",
+          isSource: true,
+          original: {
+            value: "#CCCCCC",
+          },
+          name: "light",
+          attributes: {},
+          path: ["color", "base", "gray", "light"],
+        },
+        medium: {
+          value: "#999999",
+          filePath: "tokens/colors/base.json",
+          isSource: true,
+          original: {
+            value: "#999999",
+          },
+          name: "medium",
+          attributes: {},
+          path: ["color", "base", "gray", "medium"],
+        },
+        dark: {
+          value: "#111111",
+          filePath: "tokens/colors/base.json",
+          isSource: true,
+          original: {
+            value: "#111111",
+          },
+          name: "dark",
+          attributes: {},
+          path: ["color", "base", "gray", "dark"],
+        },
+      },
+      red: {
+        value: "#FF0000",
+        filePath: "tokens/colors/base.json",
+        isSource: true,
+        original: {
+          value: "#FF0000",
+        },
+        name: "red",
+        attributes: {},
+        path: ["color", "base", "red"],
+      },
+      green: {
+        value: "#00FF00",
+        filePath: "tokens/colors/base.json",
+        isSource: true,
+        original: {
+          value: "#00FF00",
+        },
+        name: "green",
+        attributes: {},
+        path: ["color", "base", "green"],
+      },
+    },
+    font: {
+      base: {
+        value: "#FF0000",
+        filePath: "tokens/colors/font.json",
+        isSource: true,
+        original: {
+          value: "{color.base.red.value}",
+        },
+        name: "base",
+        attributes: {},
+        path: ["color", "font", "base"],
+      },
+      secondary: {
+        value: "#00FF00",
+        filePath: "tokens/colors/font.json",
+        isSource: true,
+        original: {
+          value: "{color.base.green.value}",
+        },
+        name: "secondary",
+        attributes: {},
+        path: ["color", "font", "secondary"],
+      },
+      tertiary: {
+        value: "#CCCCCC",
+        filePath: "tokens/colors/font.json",
+        isSource: true,
+        original: {
+          value: "{color.base.gray.light.value}",
+        },
+        name: "tertiary",
+        attributes: {},
+        path: ["color", "font", "tertiary"],
+      },
+    },
+  },
+  size: {
+    font: {
+      small: {
+        value: "0.75",
+        comment: "the small size of the font",
+        filePath: "tokens/size/font.json",
+        isSource: true,
+        original: {
+          value: "0.75",
+          comment: "the small size of the font",
+        },
+        name: "small",
+        attributes: {},
+        path: ["size", "font", "small"],
+      },
+      medium: {
+        value: "1",
+        comment: "the medium size of the font",
+        filePath: "tokens/size/font.json",
+        isSource: true,
+        original: {
+          value: "1",
+          comment: "the medium size of the font",
+        },
+        name: "medium",
+        attributes: {},
+        path: ["size", "font", "medium"],
+      },
+      large: {
+        value: "2",
+        comment: "the large size of the font",
+        filePath: "tokens/size/font.json",
+        isSource: true,
+        original: {
+          value: "2",
+          comment: "the large size of the font",
+        },
+        name: "large",
+        attributes: {},
+        path: ["size", "font", "large"],
+      },
+      base: {
+        value: "1",
+        comment: "the base size of the font",
+        filePath: "tokens/size/font.json",
+        isSource: true,
+        original: {
+          value: "{size.font.medium.value}",
+          comment: "the base size of the font",
+        },
+        name: "base",
+        attributes: {},
+        path: ["size", "font", "base"],
+      },
+    },
+  },
+}
