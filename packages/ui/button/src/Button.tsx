9adb2128b0a040491c75335e3abeb400c1a0ded0
Dennis <dennis.spierenburg@hotmail.com>
2022-11-08 08:01:30 +0100

    fix: Small color change

--- packages/ui/button/src/Button.tsx
+++ packages/ui/button/src/Button.tsx
@@ -8,7 +8,7 @@ export function Button({
 }: ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
     <button
-      className={`${className} text-colors-brand-secondary-50`}
+      className={`${className} text-colors-brand-secondary-100`}
       {...props}
       type={type}
     >
